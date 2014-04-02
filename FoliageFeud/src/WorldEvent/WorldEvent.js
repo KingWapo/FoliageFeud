f28998561848aade7493ab2690858e48037bbae1
KingWapo <Holcombea9172@my.uwstout.edu>
2014-04-02 11:16:51 -0500

    World Event Additions
    still broken :/

--- FoliageFeud/src/WorldEvent/WorldEvent.js
+++ FoliageFeud/src/WorldEvent/WorldEvent.js
@@ -148,6 +148,7 @@ var worldEvent = {
 		this.questionBeingAsked = false;
 		utility.clearClickHandler();
 		this.resetQuestions();
+		console.debug("AnswerQuestion Being Called");
 	},
 	
 	resetQuestions: function()
@@ -162,9 +163,9 @@ var worldEvent = {
 	renderQuestion: function()
 	{
 		utility.writeText(menuSurface, [this.questions[0].name, this.questions[1].name, this.questions[2].name], gameplayCanvas.width - 480, gameplayCanvas.height / 4 + 32, 256, 16);
-		utility.addClickItem(gameplayCanvas.width - 480, gameplayCanvas.height / 4 + 16, 256, 16, this.answerQuestion(0))
-		utility.addClickItem(gameplayCanvas.width - 480, gameplayCanvas.height / 4 + 48, 256, 16, this.answerQuestion(1))
-		utility.addClickItem(gameplayCanvas.width - 480, gameplayCanvas.height / 4 + 80, 256, 16, this.answerQuestion(2))
+		utility.addClickItem(gameplayCanvas.width - 480, gameplayCanvas.height / 4 + 16, 256, 16, this.answerQuestion)
+		utility.addClickItem(gameplayCanvas.width - 480, gameplayCanvas.height / 4 + 48, 256, 16, this.answerQuestion)
+		utility.addClickItem(gameplayCanvas.width - 480, gameplayCanvas.height / 4 + 80, 256, 16, this.answerQuestion)
 		menuSurface.drawImage(
 			this.correctImage,
 			gameplayCanvas.width / 2 - 64, gameplayCanvas.height / 4,
