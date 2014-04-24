c43ee86077554bb9c38f21426398163c8b4dd7f7
KingWapo <Holcombea9172@my.uwstout.edu>
2014-04-24 15:51:03 -0500

    Fixed Brandon's Shit.....again

--- FoliageFeud/src/MainGameplay/Gameplay.js
+++ FoliageFeud/src/MainGameplay/Gameplay.js
@@ -328,6 +328,11 @@ var gameplay = {
 		this.mainCamp.height = 128;
 		
 		this.updateSprite();
+		
+		moveUp = false;
+		moveDown = false;
+		moveLeft = false;
+		moveRight = false;
 
 	},
 	
@@ -525,7 +530,7 @@ var gameplay = {
 	update: function()
 	{
 		
-	
+		
 		if (this.onPause)
 		{
 			if (!this.mapBuilt)
