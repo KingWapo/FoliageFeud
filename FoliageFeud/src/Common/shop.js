fbd44d8b7348e4eccf8f53479b00cda9ee0c59c2
KingWapo <Holcombea9172@my.uwstout.edu>
2014-05-06 17:21:16 -0500

    Shop and camp changes

--- FoliageFeud/src/Common/shop.js
+++ FoliageFeud/src/Common/shop.js
@@ -139,11 +139,11 @@ render:function()
 				);
 			}
 	//decides the sprite for your brother.			
-	if (currentSprite == SpriteState.Boy || currentSprite == SpriteState.Girl)
+	if (originalSprite == SpriteState.Boy || originalSprite == SpriteState.Girl)
 	{
 		this.siblingSprite = imgShopSibling;
 	}
-	else if (currentSprite == SpriteState.Boy2 || currentSprite == SpriteState.Girl2)
+	else if (originalSprite == SpriteState.Boy2 || originalSprite == SpriteState.Girl2)
 	{
 		this.siblingSprite = imgShopSibling2;
 	}
